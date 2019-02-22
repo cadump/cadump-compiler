3c1d5695fbd48db5a9a47383458f7fdba347a0fb
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-02-22 20:33:35 +0100

    Add some comments

--- src/importer.hpp
+++ src/importer.hpp
@@ -4,14 +4,16 @@
 #include <fstream>
 //This class creates instance of itself for every file importing
 //Then searches the file for 'import' keywords and uses them for creating for recursive importing
-//TODO: make array of every file importing and search them first before creating another instance
+//TODO: make array of every file that is being imported and search them first before creating another instance
 
 class Importer
 {
-  std::fstream master_file;
+  //This file is used for satisfying all the imports and inserting them into one file, later returned
+  std::fstream* master_file;
 
+  //This class searches for the files that should be imported and returns their full path
   //TODO: substitute relative paths to importing files to absolute paths (in case of multiple imports with same names)
-  char* satisfy_import_scope();
+  char* satisfy_import_scope(char* file_name, char mode); //Modes are l (for local) and g (for global)
 
 public:
   Importer(std::ifstream* entry_file);
