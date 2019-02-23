a2dca3303023dffec3a9f1d58a0214422da22caf
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-02-23 21:40:04 +0100

    Add filestream that keeps its path

---
+++ lib/namedfstream.hpp
@@ -0,0 +1,22 @@
+#ifndef CADUMP_LIB_NAMEDFSTREAM_H_
+#define CADUMP_LIB_NAMEDFSTREAM_H_
+#include <fstream>
+#include <string>
+
+namespace CadumpLibs
+{
+  class NamedFstream : public std::fstream
+  {
+   std::string filename_;
+
+   public:
+     NamedFstream(const char* filename, ios_base::openmode mode = ios_base::in | ios_base::out) : std::fstream(filename, mode)
+     {
+       std::string tmp(filename);
+       this->filename_ = tmp;
+     }
+
+     std::string getFilename() {return this->filename_;}
+  };
+}
+#endif
