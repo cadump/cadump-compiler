3100b98a9da4d1b9f4619438feedab9e44816e38
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-03-01 17:06:26 +0100

    Add Import Entries and do some rfactoring

--- src/importer.hpp
+++ src/importer.hpp
@@ -4,8 +4,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <thread>
-#include <future>
 
 #include "../lib/namedfstream.hpp"
 
@@ -29,17 +27,27 @@ class Importer
     char mode;
   }typedef ImportEntry;
 
+  class ImportList : public std::vector<ImportEntry>
+  {
+    public:
+      std::string destination_object;
+  };
+
   CadumpLibs::NamedFstream* entry_file_;
 
-  std::string get_next_import_command();
+  std::string get_whole_import_command(std::string import_keyword_line);
 
-  std::vector<ImportEntry>* parse_imports(std::string import_command);
+  ImportList parse_imports(std::string import_command);
 
   //This class searches for the files that should be imported and returns their full path
   //TODO: substitute relative paths to importing files to absolute paths (in case of multiple imports with same names)
-  std::string satisfy_import_scope(ImportEntry import_entry);
+  ImportEntry satisfy_import_scope(ImportEntry import_entry);
 
-  CadumpLibs::NamedFstream* spawn_if_doesnt_exist();
+  CadumpLibs::NamedFstream* spawn_if_doesnt_exist(ImportEntry import_entry);
+
+  std::string merge_filestreams (std::vector<CadumpLibs::NamedFstream*> filestreams, std::string separator);
+
+  void write_to_master_file(std::string input);
 
 public:
   Importer(CadumpLibs::NamedFstream* entry_file);
