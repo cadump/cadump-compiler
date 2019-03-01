784c715aa2e84ca167ad5cd224be61c9b500c326
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-03-01 17:05:47 +0100

    Add seekline method (not very efficient)

--- lib/namedfstream.hpp
+++ lib/namedfstream.hpp
@@ -1,6 +1,7 @@
 #ifndef CADUMP_LIB_NAMEDFSTREAM_H_
 #define CADUMP_LIB_NAMEDFSTREAM_H_
 #include <fstream>
+#include <sstream>
 #include <string>
 
 namespace CadumpLibs
@@ -16,6 +17,22 @@ namespace CadumpLibs
      }
 
      std::string getFilename() {return this->filename_;}
+
+    std::string peekline()
+    {
+      bool is_before_linebreak = true;
+      std::string line = "";
+      while ( is_before_linebreak )
+	{
+	  char read_char = this->peek();
+	  if( read_char == '\n' || read_char == '\r')
+	    is_before_linebreak = false;
+	  else
+	    line += read_char;
+	}
+
+      return line;
+    }
   };
 }
 #endif
